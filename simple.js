191f686fab9fbbf18d0d4a182e929c4cf7544557
Shiva1029 <Shiva1029@users.noreply.github.com>
2017-10-28 14:52:38 -0400

    Update simple.js

--- simple.js
+++ simple.js
@@ -84,3 +84,30 @@ function get(url, sucessCallback, errorCallback) {
     }
     xhr.send(null);
 }
+
+function setCookie(name, val, days) {
+    var d = new Date();
+    d.setTime(d.getTime() + (days*24*60*60*1000));
+    var expires = 'expires='+ d.toUTCString();
+    document.cookie = name + '=' + val + ';' + expires + ';path=/';
+}
+
+function getCookie(name) {
+    var name = name + '=';
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return '';
+}
+
+function deleteCookie(name) {
+  document.cookie = name + '=;expires=' + new Date(1970, 0, 1).toUTCString() + ';path=/'
+}
